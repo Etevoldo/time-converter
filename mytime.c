4376db6d8914e8fd75a4264f11ec73da80e0d50a
voldo <filipecavoldo@outlook.com>
2023-02-06 09:42:33 -0300

    added minutes_convert

--- mytime.c
+++ mytime.c
@@ -1,11 +1,18 @@
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "mytime.h"
+
+#define MIN_IN_HOUR 60
+#define MAX_TIME_SIZE 11
 
 char *convert_time(int user_time, char time12h[]){
         int hour, minute;
 
-        hour   = user_time / 60;
-        minute = user_time % 60;
+        hour   = user_time / MIN_IN_HOUR;
+        minute = user_time % MIN_IN_HOUR;
 
         if (hour > 12)
                 hour -= 12;
@@ -14,4 +21,32 @@ char *convert_time(int user_time, char time12h[]){
                 hour, minute, (user_time >= 720) ? "p.m." : "a.m.");
 
         return time12h;
+}
+
+int minutes_convert(const char string[]){
+	char *s, copy[MAX_TIME_SIZE];
+	int min, hr, i = 0;
+	bool is_pm = false, is_am = false;
+
+	if (strlen(string) > MAX_TIME_SIZE - 1)
+		return -1;
+
+	s = strcpy(copy, string);
+	while (*s){                                               //upper case the copy
+		*s = toupper((unsigned int) *s);
+		s++;
+	}
+	
+	if ((s = strchr(copy, 'P')) != NULL && (*(s + 1) == 'M'))
+		is_pm = true;
+	if ((s = strchr(copy, 'A')) != NULL && (*(s + 1) == 'M'))
+		is_am = true;
+
+	sscanf(copy, "%2d%*[^0123456789]%2d", &hr, &min);
+	if (hr == 12)
+		is_pm = false;                                    //in case of 12:xxPM date situations
+	if (min >= 60 || (is_pm && hr >= 13) || (is_am && hr >= 12) || hr >= 24)  //invalid hours
+		return -1;
+	
+	return (hr * MIN_IN_HOUR) + min + (is_pm * 720);
 }
--- mytime.h
+++ mytime.h
@@ -7,6 +7,13 @@
  *               character array with at minimum 11 elements, which will be filled  *
  *               with the format described here (also returning a pointer to it)    *
  ************************************************************************************/
-char * convert_time(int, char []);
+char *convert_time(int, char []);
 
+/************************************************************************************
+ * minutes_convert: returns minutes after midnight from a variety of formats,       *
+ * 		    like 09hr30min PM, 09:30pm, 09:30am 		            *
+ * 		    only argument is a string (with at maximum of 11 characters.    *
+ * 		    returns a negative value on error.    			    *
+ ***********************************************************************************/
+int minutes_convert(const char string[]);
 #endif
